307ab7bfd8f552350fad89d1f4b8d6d9a85a9521
lihuabing-hk <lhb@mail.dangminhhoa.edu.vn>
2025-11-20 10:58:03 +0800

    Update script to use absolute path for index.js

--- container/nodejs/start.sh
+++ container/nodejs/start.sh
@@ -19,7 +19,7 @@ export agk=${agk}
 export ippz=${ippz}
 export warp=${warp}
 export name=${name}
-node container/nodejs/index.js
+node /app/container/nodejs/index.js
 v46url="https://icanhazip.com"
 showmode(){
 echo "Argosbx脚本项目地址：https://github.com/yonggekkk/argosbx"
